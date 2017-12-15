cb9160ca003fb229f7608ec7865bae3035e74d23
root <root@vmd16401.contaboserver.net>
2017-12-15 22:54:47 +0100

    another fix on encrypted file

--- .travis/prepare-gpg.sh
+++ .travis/prepare-gpg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if ( [ "$TRAVIS_BRANCH" = 'master' ] || [ ! -z "$TRAVIS_TAG" ] ) && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -K $encrypted_9ffc84d5f72f_key -iv $encrypted_9ffc84d5f72f_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_5f9c00905447_key -iv $encrypted_5f9c00905447_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
 	gpg --fast-import .travis/codesigning.asc
 fi
