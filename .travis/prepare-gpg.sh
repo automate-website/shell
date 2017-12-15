209b0beedd30d0d4a5945e63deffe48ab0da0194
build-failure <michael@lewkowski.de>
2017-12-15 22:41:24 +0100

    reencrypt the file

--- .travis/prepare-gpg.sh
+++ .travis/prepare-gpg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if ( [ "$TRAVIS_BRANCH" = 'master' ] || [ ! -z "$TRAVIS_TAG" ] ) && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -K $encrypted_9ffc84d5f72f_key -iv $encrypted_9ffc84d5f72f_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_9ffc84d5f72f_key -iv $encrypted_9ffc84d5f72f_iv -in codesigning.asc.enc -out codesigning.asc -d
 	gpg --fast-import .travis/codesigning.asc
 fi
